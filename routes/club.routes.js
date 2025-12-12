bcfd40b95a9e07f7df6638137868f2c56f307266
nomankhan232 <nomankhan192069@gmail.com>
2025-12-12 20:36:46 +0600

    club.routes update

--- routes/club.routes.js
+++ routes/club.routes.js
@@ -3,6 +3,7 @@ import {
     createClub,
     getAllClubs,
     approveClub,
+    getClubById
 } from "../controllers/club.controller.js";
 import verifyFirebase from "../middleware/verifyFirebase.js";
 import checkRole from "../middleware/checkRole.js";
@@ -10,7 +11,11 @@ import checkRole from "../middleware/checkRole.js";
 const router = express.Router();
 
 router.post("/create", verifyFirebase, createClub);
+
 router.get("/", getAllClubs);
+
+router.get("/:id", getClubById);
+
 router.patch("/approve/:id", verifyFirebase, checkRole(["admin"]), approveClub);
 
 export default router;
