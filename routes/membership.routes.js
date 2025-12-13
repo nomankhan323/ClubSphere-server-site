90d3f454ec9fba2a1e40478dae0ea9a465d5f928
nomankhan232 <nomankhan192069@gmail.com>
2025-12-13 13:32:33 +0600

    membership routes update

--- routes/membership.routes.js
+++ routes/membership.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { joinClub, getMemberships } from "../controllers/membership.controller.js";
+import verifyFirebase from "../middleware/verifyFirebase.js";
+import checkRole from "../middleware/checkRole.js";
 
 const router = express.Router();
 
-router.post("/join", joinClub);
-router.get("/", getMemberships);
+router.post("/join", verifyFirebase, checkRole(["member"]), joinClub);
+
+router.get("/", verifyFirebase, getMemberships);
 
 export default router;
