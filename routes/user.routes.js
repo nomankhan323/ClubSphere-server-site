83b53e30c8c38231ae9d3fdd9fcb949673e96ad3
nomankhan232 <nomankhan192069@gmail.com>
2025-12-13 13:23:32 +0600

    user routes update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,23 @@
 import express from "express";
-import { saveUser, getUsers } from "../controllers/user.controller.js";
+import {
+    saveUser,
+    getUsers,
+    updateUserRole,
+} from "../controllers/user.controller.js";
 import verifyFirebase from "../middleware/verifyFirebase.js";
+import checkRole from "../middleware/checkRole.js";
 
 const router = express.Router();
 
-router.post("/save", saveUser);
+router.post("/save", verifyFirebase, saveUser);
 
-router.get("/", verifyFirebase, getUsers);
+router.get("/", verifyFirebase, checkRole(["admin"]), getUsers);
+
+router.patch(
+    "/:id/role",
+    verifyFirebase,
+    checkRole(["admin"]),
+    updateUserRole
+);
 
 export default router;
