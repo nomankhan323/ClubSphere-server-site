08bafab6dcad3fa34eebdfca4aeb3553f8039480
nomankhan232 <nomankhan192069@gmail.com>
2025-12-13 13:24:31 +0600

    Club.js update

--- models/Club.js
+++ models/Club.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 const clubSchema = new mongoose.Schema({
-    name: String,
+    clubName: String,
     description: String,
+    category: String,
+    location: String,
+    bannerImage: String,
+    membershipFee: { type: Number, default: 0 },
     status: { type: String, default: "pending" },
     managerEmail: String,
+    createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model("Club", clubSchema);
