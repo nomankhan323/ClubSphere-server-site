78137ff9a1c3f91f17cc50a58e8823a5deb58494
nomankhan232 <nomankhan192069@gmail.com>
2025-12-13 13:29:29 +0600

    membership controller update

--- controllers/membership.controller.js
+++ controllers/membership.controller.js
@@ -1,11 +1,24 @@
 import Membership from "../models/Membership.js";
 
 export const joinClub = async (req, res) => {
-    const membership = await Membership.create(req.body);
+    const email = req.dbUser.email;
+    const { clubId } = req.body;
+
+    const exists = await Membership.findOne({ clubId, userEmail: email });
+
+    if (exists) {
+        return res.status(400).json({ message: "Already joined" });
+    }
+
+    const membership = await Membership.create({
+        clubId,
+        userEmail: email,
+    });
+
     res.json(membership);
 };
 
 export const getMemberships = async (req, res) => {
-    const data = await Membership.find();
+    const data = await Membership.find({ userEmail: req.dbUser.email });
     res.json(data);
 };
